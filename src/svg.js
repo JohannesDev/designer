520fb98066149a9f042bbb92268522752286fcd9
Johannes <33333384+JohannesDev@users.noreply.github.com>
2019-07-01 23:35:31 +0200

    working with svelte

---
+++ src/svg.js
@@ -0,0 +1,117 @@
+let canvas
+let ctx
+var objectList = []
+
+export class SVG {
+  constructor() {
+    canvas = document.getElementById("drawing")
+    ctx = canvas.getContext("2d")
+  }
+
+  add(object) {
+    objectList.push(object)
+  }
+
+  remove(object) {
+    let index = objectList.indexOf(object)
+    objectList.splice(index, 1);
+
+  }
+
+  checkIfObjectClicked(point) {
+    console.log(objectList[0]);
+
+    let state = ctx.isPointInStroke(point.x, point.y);
+    console.log(state);
+    
+  }
+
+
+  redraw() {
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    if (objectList.length > 0) {
+      objectList.forEach(function (object, index) {
+        switch (objectList[index].constructor.name) {
+          case "Line":
+            new Line(object.startPoint, object.endPoint)
+            break
+          case "Rect":
+            new Rect(object.startPoint, object.endPoint)
+            break
+          case "Ellipse":
+            new Ellipse(object.startPoint, object.endPoint)
+            break
+        }
+
+        ctx.lineCap = 'round'
+        ctx.lineWidth = 8
+        //ctx.fillStyle = "red";
+        //ctx.fill();
+        ctx.stroke();
+      })
+
+    }
+
+
+
+
+  }
+
+}
+
+
+export class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+export class Line {
+  constructor(startPoint, endPoint) {
+    this.startPoint = startPoint;
+    this.endPoint = endPoint;
+
+    ctx.beginPath()
+    ctx.moveTo(startPoint.x, startPoint.y)
+    ctx.lineTo(endPoint.x, endPoint.y)
+    ctx.moveTo(endPoint.x, endPoint.y)
+  }
+}
+
+
+export class Rect {
+  constructor(startPoint, endPoint) {
+    this.startPoint = startPoint;
+    this.endPoint = endPoint;
+
+    ctx.beginPath()
+    ctx.moveTo(startPoint.x, startPoint.y)
+
+    ctx.lineTo(endPoint.x, startPoint.y)
+    ctx.lineTo(endPoint.x, endPoint.y)
+    ctx.lineTo(startPoint.x, endPoint.y)
+    ctx.lineTo(startPoint.x, startPoint.y)
+
+    ctx.closePath()
+
+  }
+}
+
+export class Ellipse {
+  constructor(startPoint, endPoint) {
+    this.startPoint = startPoint;
+    this.endPoint = endPoint;
+
+    let radiusx = Math.abs((endPoint.x - startPoint.x)/2)
+    let radiusy = Math.abs((endPoint.y - startPoint.y)/2)
+
+    ctx.beginPath()
+    ctx.arcTo(startPoint.x, startPoint.y, radiusx, radiusy, 5);
+
+  }
+}
+
+
