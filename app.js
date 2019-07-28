4d007aad1cf4f8e5f0d7ef0f3b56b9184ad4550d
Johannes <j.blaha.htl@gmail.com>
2019-07-28 10:24:37 +0200

    SVG working

---
+++ app.js
@@ -0,0 +1,30 @@
+
+let drawingHelper = new DrawingHelper();
+
+//drawingHelper.drawRect(10,10,50,50)
+
+drawingHelper.drawRect(120,120,50,50)
+
+
+
+
+$('drawing').addEventListener('mousedown', (event) => {
+    if (event.target.instance.type === "svg") {
+    
+    }
+});
+
+$('drawing').addEventListener('mousemove', () => {
+    if (event.target.instance.type === "svg") {
+    }
+    
+});
+
+$('drawing').addEventListener('mouseup', () => {
+
+});
+
+
+
+
+
